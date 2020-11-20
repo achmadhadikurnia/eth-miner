bce6dd8bb27a2712cccb382ba00df9913771110c
achmadhadikurnia <imachmadhadikurnia@gmail.com>
2020-11-20 07:40:06 +0700

    Add T-Rex

---
+++ trex.sh
@@ -0,0 +1,7 @@
+# Bash for ethermine.org pool
+/content/watersprings/trex -a ethash -o stratum+tcp://asia1.ethermine.org:4444 -u 0x614800397a8f503cc157ec9b8f81356fa74e3af6 -p x -w WaterSprings
+/content/watersprings/trex -a ethash -o stratum+tcp://eu1.ethermine.org:4444 -u 0x614800397a8f503cc157ec9b8f81356fa74e3af6 -p x -w WaterSprings
+/content/watersprings/trex -a ethash -o stratum+tcp://asia1.ethermine.org:4444 -u 0x614800397a8f503cc157ec9b8f81356fa74e3af6 -p x -w WaterSprings
+/content/watersprings/trex -a ethash -o stratum+tcp://asia1.ethermine.org:4444 -u 0x614800397a8f503cc157ec9b8f81356fa74e3af6 -p x -w WaterSprings
+/content/watersprings/trex -a ethash -o stratum+tcp://asia1.ethermine.org:4444 -u 0x614800397a8f503cc157ec9b8f81356fa74e3af6 -p x -w WaterSprings
+/content/watersprings/trex -a ethash -o stratum+tcp://asia1.ethermine.org:4444 -u 0x614800397a8f503cc157ec9b8f81356fa74e3af6 -p x -w WaterSprings
