74c95f7d00c6ee9a554e983d542567e777622331
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2021-08-01 14:21:38 +0700

    eth address to TokoCrypto

--- b_ethermine.sh
+++ b_ethermine.sh
@@ -1,5 +1,5 @@
 # IDENTITY
-WALLET=0x614800397a8f503cc157ec9b8f81356fa74e3af6
+WALLET=0xfcf81487ca6da108e4856d5bc8f50e05f18ab288
 ENGINE_NAME=B_WaterSprings_
 UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 RIG_NAME=$ENGINE_NAME$UUID
